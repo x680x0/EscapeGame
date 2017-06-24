8a75a1613eabbd9329594aad1e97d71972b25833
minamino <wfs91-days@ezweb.ne.jp>
2017-06-25 03:15:20 +0900

    スライム(攻撃モーションなどの設定)

--- Assets/Scripts/objectBase.cs
+++ Assets/Scripts/objectBase.cs
@@ -2,27 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class objectBase: MonoBehaviour {
+public class objectBase:MonoBehaviour {
     [System.NonSerialized]
     public SpriteRenderer spriteRenderer;
-   // [System.NonSerialized]
+    // [System.NonSerialized]
     public float once;
     public int X, Y;
     public int MAXHP, HP;
     [System.NonSerialized]
-    public int readyX,readyY;
+    public int readyX, readyY;
     public GameObject[] pivot;//光に入っているか判定する座標
     [System.NonSerialized]
     public mgrScript MGR;//入力や全体を通しての初期値などはここから
     public bool inLight;//光の当たる範囲
     public int vector;//上から時計回りに0123
-    public  bool stop;
+    public bool stop;
     public enum typeOfDamage {
-        cross=0,
+        cross = 0,
         mid
     }
-	// Use this for initialization
-	public virtual void Start () {
+    // Use this for initialization
+    public virtual void Start() {
         inLight = false;
         MGR = GameObject.Find("mgrObject").GetComponent<mgrScript>();
         stop = true;
@@ -31,37 +31,37 @@ public class objectBase: MonoBehaviour {
         X = (int)(transform.localPosition.x / once);
         Y = (int)(transform.localPosition.y / once);
     }
-	
-	// Update is called once per frame
-	public virtual void Update () {
+
+    // Update is called once per frame
+    public virtual void Update() {
         spriteRenderer.sortingOrder = -Y;
-        transform.localPosition = new Vector3(X *once, Y * once, transform.localPosition.z);
+        transform.localPosition = new Vector3(X * once, Y * once, transform.localPosition.z);
     }
     public virtual void FixedUpdate() {
-       
+
     }
 
-    public virtual void Damaged(int damage,typeOfDamage type) {
-        Destroy(this.gameObject);
+    public virtual void Damaged(int damage, typeOfDamage type) {
+      //  Destroy(this.gameObject);
         HP -= damage;
         if(HP < 0) {
             Death();
         }
     }
 
-   public virtual void Move() {//実際に動かす　SetMoveで事前にセットしておくこと
+    public virtual void Move() {//実際に動かす　SetMoveで事前にセットしておくこと
         if(readyX * readyY == 0) {
             X += readyX;
             Y += readyY;
-        }else {
-            X += (int)(readyX/1.4);
-            Y += (int)(readyY/1.4);
+        } else {
+            X += (int)(readyX / 1.4);
+            Y += (int)(readyY / 1.4);
         }
     }
-    public virtual void SetVector(int _vector,int speed) {//これでSetしてMoveで実際に動かす。
+    public virtual void SetVector(int _vector, int speed) {//これでSetしてMoveで実際に動かす。
         switch(_vector) {
             case 0:
-                readyY +=speed;
+                readyY += speed;
                 break;
             case 1:
                 readyX += speed;
@@ -70,16 +70,57 @@ public class objectBase: MonoBehaviour {
                 readyY -= speed;
                 break;
             case 3:
-                readyX -=speed;
+                readyX -= speed;
                 break;
         }
     }
     public virtual void Death() { }
-    public virtual void SetPosition(int _X,int _Y) {
-       
+    public virtual void SetPosition(int _X, int _Y) {
+
         X = _X;
         Y = _Y;
         transform.localPosition = new Vector3(X * once, Y * once, transform.localPosition.z);
 
+    }
+    public void GetVector(int X1, int Y1, int X2, int Y2, ref int V1, ref int V2) {
+        if(X1 <= X2) {
+            V1 = 1;
+        } else {
+            V1 = 3;
+        }
+        if(Y1 <= Y2) {
+            V1 = 0;
+        } else {
+            V1 = 2;
+        }
+    }
+    public virtual void Attack(GameObject _pivot,int damage,typeOfDamage type) {
+        Collider2D[][] damagedObjectCollider = new Collider2D[1][];
+        /* damagedObjects = Physics2D.OverlapPointAll(_pivot.transform.localPosition);
+        
+         foreach(Collider2D damagedObject in damagedObjects) {  
+             if(damagedObject != null) {
+                 if(!damagedObject.isTrigger) {
+
+                 }
+             }
+         }*/
+        objectBase damagedScript;
+        damagedObjectCollider[0] = Physics2D.OverlapPointAll(_pivot.transform.position);
+        foreach(Collider2D[] damagedObjectList in damagedObjectCollider) {
+            foreach(Collider2D damagedObject in damagedObjectList) {
+                if(damagedObject != null) {
+                    if(!damagedObject.isTrigger) {
+                        damagedScript = damagedObject.gameObject.transform.parent.gameObject.GetComponent<objectBase>();
+                        if(damagedScript != null) {
+                            damagedScript.Damaged(damage, type);
+                           
+
+                        }
+                    }
+                }
+
+            }
+        }
     }
 }
---
+++ Assets/Scripts/slimeScript.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slimeScript:objectBase {
+    static int[] walk,attack;
+    float time;
+    Animator animator;
+    public GameObject target;
+    objectBase targetScript;
+    public GameObject[] attackpivot;
+    public bool setAttack;
+    public override void Start() {
+        base.Start();
+        setAttack = false;
+        animator = GetComponent<Animator>();
+        walk = new int[4];
+        walk[0] = Animator.StringToHash("up");
+        walk[1] = Animator.StringToHash("right");
+        walk[2] = Animator.StringToHash("down");
+        walk[3] = Animator.StringToHash("left");
+        attack = new int[4];
+        attack[0] = Animator.StringToHash("attackup");
+        attack[1] = Animator.StringToHash("attackright");
+        attack[2] = Animator.StringToHash("attackdown");
+        attack[3] = Animator.StringToHash("attackleft");
+        time = 0;
+        if(target != null) {
+            targetScript = target.GetComponent<objectBase>();
+        }
+    }
+    public override void Update() {
+        spriteRenderer.sortingOrder = -Y;
+        transform.localPosition = new Vector3(X * once, (Y+22) * once, transform.localPosition.z);
+    }
+    public override void FixedUpdate() {
+        inLight = false;
+        Collider2D[][] groundCheckCollider = new Collider2D[1][];
+        groundCheckCollider[0] = Physics2D.OverlapPointAll(pivot[vector].transform.position);
+        foreach(Collider2D[] groundCheckList in groundCheckCollider) {
+            foreach(Collider2D groundCheck in groundCheckList) {
+                if(groundCheck != null) {
+                    if(!groundCheck.isTrigger) {
+                        if(groundCheck.tag == "Light") {
+                            inLight = true;
+                        }
+                    }
+                }
+
+            }
+        }
+        int D1 =X - targetScript.X;
+        int D2 =Y - targetScript.Y;
+        if(!setAttack) {
+            if(target == null) {
+
+            } else {
+                readyX = 0; readyY = 0;//初期化
+                time += Random.Range(0.05f, 0.1f);
+                if(time > 1f) {
+                    int V1, V2;
+                    float A = Mathf.Atan2(D2, D1);
+                    A *= Mathf.Rad2Deg;
+                    if(0 == Random.Range(0, 5)&&D1 < 150&&D2<150) {
+                        if(-45<A&&A<45) {
+                            vector = 3;
+                        } else if(45<A&&A<135) {
+                            vector = 2;
+                        } else if(-135<A&&A<-45) {
+                            vector = 0;
+                        } else {
+                            vector = 1;
+                        }
+                        setAttack = true;
+                    }
+                    if(!setAttack) {
+                        if(0 == Random.Range(0, 3) && (Mathf.Abs(D1) > 100 || Mathf.Abs(D2) > 100)) {
+                            if(D1 < 0) {
+                                V1 = 1;
+                            } else {
+                                V1 = 3;
+                            }
+                            if(D2 < 0) {
+                                V2 = 0;
+                            } else {
+                                V2 = 2;
+                            }
+                            if(Mathf.Abs(D1) > Mathf.Abs(D2)) {
+                                vector = V1;
+                            } else {
+                                vector = V2;
+                            }
+
+
+
+
+                        } else {
+                            vector = Random.Range(0, 4);
+                        }
+                    }
+                    time = 0;
+                }
+                if(setAttack) {
+                    animator.Play(attack[vector]);
+                } else {
+                    animator.Play(walk[vector]);
+                    SetVector(vector, Random.Range(4,7));
+                    if(4 > Random.Range(0, 10)) {
+                  
+                    }else {
+                        Move();
+
+                    }
+
+                }
+            }
+        }
+    }
+
+    public void SlimeAttack() {
+        int count=0;
+        foreach(Transform child in attackpivot[vector].transform) {
+            //child is your child transform
+            Attack(child.gameObject, 1, typeOfDamage.cross);
+            count++;
+        }
+    }
+}
