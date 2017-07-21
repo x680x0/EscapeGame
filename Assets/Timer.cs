2e521d7e68c5f46853f93b1f087feaf74a1ba677
60606060 <pepu60minamino@gmail.com>
2017-07-22 02:17:40 +0900

    時間
    時間を表示

---
+++ Assets/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer:MonoBehaviour {
+
+    public Image gage;
+    public GameObject player;
+    lightScript objec;
+    // Use this for initialization
+    void Start() {
+        objec = player.GetComponent<lightScript>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float tmp = (float)objec.time / (float)20;
+        gage.fillAmount = tmp;
+    }
+}
